555e834e11ca8e4c3985ef99d96bd8281fd3add2
Jisu Sim <rlj1202@gmail.com>
2023-03-13 13:15:24 +0900

    Fix github actions

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : '/shake-site'
+			base: dev ? '' : '/shake-site',
+			relative: false
 		}
 	}
 };
