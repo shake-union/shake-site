f77c30caf830b36b0ab775776b0abdc00b02fed1
jong <jong@jongui-MacBookAir.local>
2026-04-30 17:59:28 +0900

    edit logo

--- src/routes/(apc)/+layout.svelte
+++ src/routes/(apc)/+layout.svelte
@@ -95,14 +95,14 @@
 		content="{base}/images/favicons/apc/mstile-310x310.png"
 	/>
 
-	<meta property="og:image" content="{base}/images/og/apc-2025.png" />
-	<meta property="og:title" content="2025년 제15회 아주대학교 프로그래밍 경시대회가 열립니다!" />
+	<meta property="og:image" content="{base}/images/og/apc-2026.png" />
+	<meta property="og:title" content="2026년 제16회 아주대학교 프로그래밍 경시대회가 열립니다!" />
 	<meta property="og:url" content="{base}/apc" />
-	<meta property="og:site_name" content="2025 APC" />
+	<meta property="og:site_name" content="2026 APC" />
 	<meta property="og:type" content="website" />
 	<meta
 		property="og:description"
-		content="2025년 5월 17일, 제15회 Ajou Programming Contest가 개최됩니다."
+		content="2026년 5월 23일, 제16회 Ajou Programming Contest가 개최됩니다."
 	/>
 </svelte:head>
 
@@ -112,7 +112,7 @@
 			<div class="col-md-10 offset-md-1">
 				<nav class="navbar navbar-expand-lg">
 					<a class="navbar-brand" href="{base}/apc">
-						<img src="{base}/logo-apc-2025.png" alt="APC" height="30px" />
+						<img src="{base}/logo-apc-2026.png" alt="APC" height="30px" />
 					</a>
 					<button
 						class="navbar-toggler"
